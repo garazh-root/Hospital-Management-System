ca9946f2b56935917b6b637f504a0e8ee7884d2c
Maksym2105 <maxdetrai02@gmail.com>
2026-02-07 15:49:45 +0200

    Added tests for ScheduleMapper class

--- doctor-service/src/test/java/org/com/doctorservice/mapper/ScheduleMapperTest.java
+++ doctor-service/src/test/java/org/com/doctorservice/mapper/ScheduleMapperTest.java
@@ -1,81 +1,129 @@
 package org.com.doctorservice.mapper;
-//
-//import org.com.doctorservice.additional.CustomDayOfTheWeek;
-//import org.junit.jupiter.api.Test;
-//
-//import java.time.LocalDate;
-//import java.time.LocalTime;
-//import java.util.List;
-//import java.util.UUID;
-//
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-//
+
+
+import org.com.doctorservice.additional.CustomDayOfTheWeek;
+import org.com.doctorservice.additional.OverrideType;
+import org.com.doctorservice.dto.ScheduleOverrideResponse;
+import org.com.doctorservice.dto.ScheduleTemplateResponse;
+import org.com.doctorservice.model.ScheduleOverride;
+import org.com.doctorservice.model.ScheduleTemplate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class ScheduleMapperTest {
 
+    private UUID id;
+    private UUID doctorId;
+
+    private ScheduleTemplate scheduleTemplate;
+    private ScheduleTemplate scheduleTemplateWithNullEffectiveFromAndTo;
+    private ScheduleOverride scheduleOverride;
+    private ScheduleTemplateResponse scheduleTemplateResponse;
+    private ScheduleOverrideResponse scheduleOverrideResponse;
+
+    @BeforeEach
+    void setUp() {
+        this.id = UUID.randomUUID();
+        this.doctorId = UUID.randomUUID();
+
+        this.scheduleTemplate = ScheduleTemplate.builder()
+                .id(id)
+                .doctorId(doctorId)
+                .customDayOfTheWeek(CustomDayOfTheWeek.MONDAY)
+                .startTime(LocalTime.of(9, 0))
+                .endTime(LocalTime.of(17, 0))
+                .breakStartTime(LocalTime.of(13, 0))
+                .breakEndTime(LocalTime.of(14, 0))
+                .slotDurationOfMinutes(30)
+                .active(true)
+                .createdAt(LocalDate.now())
+                .build();
+
+        this.scheduleTemplateWithNullEffectiveFromAndTo = ScheduleTemplate.builder()
+                .id(id)
+                .doctorId(doctorId)
+                .customDayOfTheWeek(CustomDayOfTheWeek.MONDAY)
+                .startTime(LocalTime.of(9, 0))
+                .endTime(LocalTime.of(17, 0))
+                .breakStartTime(LocalTime.of(13, 0))
+                .breakEndTime(LocalTime.of(14, 0))
+                .slotDurationOfMinutes(30)
+                .effectiveFrom(null)
+                .effectiveTo(null)
+                .active(true)
+                .createdAt(LocalDate.now())
+                .build();
+
+        this.scheduleOverride = ScheduleOverride.builder()
+                .id(id)
+                .doctorId(doctorId)
+                .date(LocalDate.of(2026, 6, 23))
+                .overrideType(OverrideType.UNAVAILABLE)
+                .startTime(LocalTime.of(9, 0))
+                .endTime(LocalTime.of(17, 0))
+                .slotDurationOfMinutes(30)
+                .reason("Sick")
+                .build();
+
+        this.scheduleTemplateResponse = ScheduleTemplateResponse.builder()
+                .id(id.toString())
+                .doctorId(doctorId.toString())
+                .dayOfTheWeek(CustomDayOfTheWeek.FRIDAY.toString())
+                .startTime(LocalTime.of(9, 0).toString())
+                .endTime(LocalTime.of(17, 0).toString())
+                .breakStartTime(LocalTime.of(13, 0).toString())
+                .breakEndTime(LocalTime.of(14, 0).toString())
+                .slotDurationOfMinutes(Integer.valueOf(30).toString())
+                .effectiveFrom(null)
+                .effectiveTo(null)
+                .build();
+
+        this.scheduleOverrideResponse = ScheduleOverrideResponse.builder()
+                .id(id.toString())
+                .doctorId(doctorId.toString())
+                .overrideType(OverrideType.UNAVAILABLE.toString())
+                .date(LocalDate.of(2026, 2, 10).toString())
+                .startTime(LocalTime.of(8, 0).toString())
+                .endTime(LocalTime.of(16, 0).toString())
+                .slotDurationOfMinutes(Integer.valueOf(30).toString())
+                .reason("Sick")
+                .build();
+    }
+
+    @Test
+    void toScheduleTemplateDTOShouldSuccessfullyMapDataToDTO() {
+        ScheduleTemplateResponse template = ScheduleMapper.toDTO(scheduleTemplate);
+
+        assertEquals(id.toString(), template.getId());
+        assertEquals(doctorId.toString(), template.getDoctorId());
+        assertEquals(CustomDayOfTheWeek.MONDAY.toString(), template.getDayOfTheWeek());
+        assertEquals(LocalTime.of(9, 0).toString(), template.getStartTime());
+        assertEquals(LocalTime.of(17, 0).toString(), template.getEndTime());
+    }
+
+    @Test
+    void toScheduleTemplateDTOShouldSuccessfullyMapDataWithNullEffectiveFromAndTo() {
+        ScheduleTemplateResponse response = ScheduleMapper.toDTO(scheduleTemplateWithNullEffectiveFromAndTo);
+
+        assertNull(null, response.getEffectiveFrom());
+        assertNull(null, response.getEffectiveTo());
+    }
+
+    @Test
+    void toScheduleOverrideDTOShouldSuccessfullyMapDataToDTO() {
+        ScheduleOverrideResponse scheduleOverrideResponse = ScheduleMapper.toDTO(scheduleOverride);
+
+        assertEquals(id.toString(), scheduleOverrideResponse.getId());
+        assertEquals(doctorId.toString(), scheduleOverrideResponse.getDoctorId());
+        assertEquals(OverrideType.UNAVAILABLE.toString(), scheduleOverrideResponse.getOverrideType());
+        assertEquals(LocalTime.of(9, 0).toString(), scheduleOverrideResponse.getStartTime());
+        assertEquals(LocalTime.of(17, 0).toString(), scheduleOverrideResponse.getEndTime());
+    }
 }
-//
-//    @Test
-//    void mapListOfSchedulesToResponseDTOShouldReturnListOfSchedules() {
-//
-//        Schedule firstSchedule = Schedule.builder()
-//                .scheduleId(UUID.randomUUID())
-//                .startTime(LocalTime.of(9, 0))
-//                .endTime(LocalTime.of(16, 0))
-//                .scheduleDate(LocalDate.of(2025, 11, 25))
-//                .breakStartTime(LocalTime.of(13, 0))
-//                .breakEndTime(LocalTime.of(14, 0))
-//                .customDayOfTheWeek(CustomDayOfTheWeek.MONDAY)
-//                .isDayOff(false)
-//                .build();
-//
-//        Schedule secondSchedule = Schedule.builder()
-//                .scheduleId(UUID.randomUUID())
-//                .startTime(LocalTime.of(10, 0))
-//                .endTime(LocalTime.of(16, 0))
-//                .scheduleDate(LocalDate.of(2025, 11, 27))
-//                .breakStartTime(LocalTime.of(13, 0))
-//                .breakEndTime(LocalTime.of(14, 0))
-//                .customDayOfTheWeek(CustomDayOfTheWeek.WEDNESDAY)
-//                .isDayOff(false)
-//                .build();
-//
-//        List<ScheduleResponseDTO> schedulesResponseDTOsList = ScheduleMapper.mapListOfSchedulesToResponseDTO(List.of(firstSchedule, secondSchedule));
-//
-//        assertThat(schedulesResponseDTOsList.size()).isEqualTo(2);
-//
-//        assertThat(schedulesResponseDTOsList.get(0).getScheduleStartTime()).isEqualTo(firstSchedule.getStartTime().toString());
-//        assertThat(schedulesResponseDTOsList.get(0).getScheduleEndTime()).isEqualTo(firstSchedule.getEndTime().toString());
-//
-//        assertThat(schedulesResponseDTOsList.get(1).getScheduleStartTime()).isEqualTo(secondSchedule.getStartTime().toString());
-//        assertThat(schedulesResponseDTOsList.get(1).getScheduleEndTime()).isEqualTo(secondSchedule.getEndTime().toString());
-//    }
-//
-//    @Test
-//    void mapScheduleToResponseDTOShouldReturnScheduleResponseDTO() {
-//
-//        Schedule schedule = Schedule.builder()
-//                .scheduleId(UUID.randomUUID())
-//                .startTime(LocalTime.of(9, 0))
-//                .endTime(LocalTime.of(16, 0))
-//                .scheduleDate(LocalDate.of(2025, 11, 25))
-//                .breakStartTime(LocalTime.of(13, 0))
-//                .breakEndTime(LocalTime.of(14, 0))
-//                .customDayOfTheWeek(CustomDayOfTheWeek.MONDAY)
-//                .isDayOff(false)
-//                .build();
-//
-//        ScheduleResponseDTO scheduleResponseDTO = ScheduleResponseDTO.builder()
-//                .scheduleStartTime(schedule.getStartTime().toString())
-//                .scheduleEndTime(schedule.getEndTime().toString())
-//                .scheduleDate(schedule.getScheduleDate().toString())
-//                .breakStartTime(schedule.getBreakStartTime().toString())
-//                .breakEndTime(schedule.getBreakEndTime().toString())
-//                .dayOfWeek(schedule.getCustomDayOfTheWeek().toString())
-//                .isDayOff(String.valueOf(schedule.isDayOff()))
-//                .build();
-//
-//        assertThat(scheduleResponseDTO.getScheduleStartTime()).isEqualTo(schedule.getStartTime().toString());
-//        assertThat(scheduleResponseDTO.getScheduleEndTime()).isEqualTo(schedule.getEndTime().toString());
-//        assertThat(scheduleResponseDTO.getScheduleDate()).isEqualTo(schedule.getScheduleDate().toString());
-//    }
-//}
