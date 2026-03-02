708383ebfcc73585593426e3a357b9500107db9c
Maksym2105 <maxdetrai02@gmail.com>
2026-03-02 18:44:39 +0200

    Changed feign client - so meeting service communicate with doctor service by url, (as they on the same network)
    Also removed old files

--- meeting-service/src/main/java/org/com/meetingservice/client/DoctorClient.java
+++ meeting-service/src/main/java/org/com/meetingservice/client/DoctorClient.java
@@ -1,10 +1,22 @@
 package org.com.meetingservice.client;
 
+import org.com.meetingservice.dto.ScheduleResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
 
 @FeignClient(
-        name = "doctor-service", fallback = DoctorClientFallback.class
+        name = "doctor-service",url = "${doctor.service.url:http://doctor-service:1011}", fallback = DoctorClientFallback.class
 )
 public interface DoctorClient {
 
+    @GetMapping("/schedule/data")
+    ScheduleResponse getSchedulesData (
+            @RequestParam String doctorId,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
+            );
 }
