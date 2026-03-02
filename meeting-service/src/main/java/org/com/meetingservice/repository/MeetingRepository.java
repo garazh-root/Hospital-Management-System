53ea342170554ac438474ca5ff13e94b33a7d3e6
Maksym2105 <maxdetrai02@gmail.com>
2026-03-02 18:46:22 +0200

    Adjusted service classes, bug fixes

--- meeting-service/src/main/java/org/com/meetingservice/repository/MeetingRepository.java
+++ meeting-service/src/main/java/org/com/meetingservice/repository/MeetingRepository.java
@@ -2,17 +2,28 @@ package org.com.meetingservice.repository;
 
 import org.com.meetingservice.additional.MeetingStatus;
 import org.com.meetingservice.model.Meeting;
-import org.springframework.data.domain.Page;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+
 
 @Repository
 public interface MeetingRepository extends MongoRepository<Meeting, String> {
 
+    List<Meeting> findByDoctorIdAndMeetingDateTimeBetween(UUID doctorId, LocalDateTime start, LocalDateTime end);
+
+    List<Meeting> findByDoctorIdAndMeetingDateTimeBetweenAndStatus(UUID doctorId, LocalDateTime start, LocalDateTime end, MeetingStatus status);
+
+    List<Meeting> findByPatientId(UUID patientId);
+
+    @Query("{'doctor_id' : ?0, 'meeting_date_time' : {$gte: ?1, $lt: ?2}, 'status': 'CONFIRMED'}")
+    List<Meeting> findScheduledMeetingsForDate(
+            UUID doctorId,
+            LocalDateTime startOfTheDay,
+            LocalDateTime endOfTheDay
+    );
 }
--- meeting-service/src/main/java/org/com/meetingservice/service/MeetingService.java
+++ meeting-service/src/main/java/org/com/meetingservice/service/MeetingService.java
@@ -1,11 +1,116 @@
 package org.com.meetingservice.service;
 
-import lombok.RequiredArgsConstructor;
+import org.com.meetingservice.additional.MeetingStatus;
+import org.com.meetingservice.client.DoctorClient;
+import org.com.meetingservice.dto.AvailableSlotResponse;
+import org.com.meetingservice.dto.ScheduleResponse;
+import org.com.meetingservice.dto.MeetingResponse;
+import org.com.meetingservice.exception.MeetingNotFoundException;
+import org.com.meetingservice.mapper.MeetingMapper;
+import org.com.meetingservice.messages.MeetingServiceMessages;
+import org.com.meetingservice.model.Meeting;
+import org.com.meetingservice.repository.MeetingRepository;
+import org.com.meetingservice.requests.MeetingRequest;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.UUID;
+
 @Service
-@RequiredArgsConstructor
 public class MeetingService {
+    private MeetingRepository meetingRepository;
+    private SlotGeneratorService slotGeneratorService;
+    private DoctorClient doctorClient;
 
+    public MeetingService(MeetingRepository meetingRepository, SlotGeneratorService slotGeneratorService,  DoctorClient doctorClient) {
+        this.meetingRepository = meetingRepository;
+        this.slotGeneratorService = slotGeneratorService;
+        this.doctorClient = doctorClient;
+    }
 
+    public List<AvailableSlotResponse> getAvailableSlots(String doctorId, LocalDate date) {
+        ScheduleResponse doctorScheduleDataResponse = doctorClient.getSchedulesData(
+                doctorId, date, date);
+
+        LocalDateTime start = date.atStartOfDay();
+        LocalDateTime end = date.atTime(LocalTime.MAX);
+
+        List<Meeting> meetingList = meetingRepository.findScheduledMeetingsForDate(UUID.fromString(doctorId), start, end);
+
+        List<LocalDateTime> bookedMeetings = meetingList
+                .stream()
+                .map(Meeting::getMeetingDateTime)
+                .toList();
+
+        return slotGeneratorService.generateAvailableSlots(doctorScheduleDataResponse, date, bookedMeetings);
+    }
+
+    public MeetingResponse bookMeeting(MeetingRequest meetingRequest) {
+        LocalDate date = meetingRequest.meetingDateTime().toLocalDate();
+
+        List<AvailableSlotResponse> availableSlots = getAvailableSlots(meetingRequest.doctorId().toString(), date);
+
+        LocalTime requestedTime = meetingRequest.meetingDateTime().toLocalTime();
+
+        boolean availableSlot = availableSlots.stream()
+                .anyMatch(slot -> LocalTime.parse(slot.startTime()).equals(requestedTime));
+
+        if (!availableSlot) {
+            throw new IllegalStateException(MeetingServiceMessages.SLOT_NOT_AVAILABLE.getMessage());
+        }
+
+        Meeting meeting = Meeting.builder()
+                .doctorId(meetingRequest.doctorId())
+                .patientId(meetingRequest.patientId())
+                .meetingDateTime(meetingRequest.meetingDateTime())
+                .durationOfMinutes(meetingRequest.duration())
+                .status(MeetingStatus.CONFIRMED)
+                .reason(meetingRequest.reason())
+                .notes("Regular checkup")
+                .build();
+
+        meetingRepository.save(meeting);
+
+        return MeetingMapper.toResponseDTO(meeting);
+    }
+
+    public List<MeetingResponse> findByDoctorIdAndDateTimeBetween(UUID doctorId, LocalDate start, LocalDate end) {
+        LocalDateTime startDate = start.atStartOfDay();
+        LocalDateTime endDate = end.atTime(LocalTime.MAX);
+
+        return meetingRepository.findByDoctorIdAndMeetingDateTimeBetween(doctorId, startDate, endDate)
+                .stream()
+                .map(MeetingMapper::toResponseDTO)
+                .toList();
+    }
+
+    public List<MeetingResponse> findByDoctorIdAndDateTimeBetweenAndStatus(UUID doctorId, LocalDate start, LocalDate end, MeetingStatus status) {
+        LocalDateTime startDate = start.atStartOfDay();
+        LocalDateTime endDate = end.atTime(LocalTime.MAX);
+
+        return meetingRepository.findByDoctorIdAndMeetingDateTimeBetweenAndStatus(doctorId, startDate, endDate, status)
+                .stream()
+                .map(MeetingMapper::toResponseDTO)
+                .toList();
+    }
+
+    public List<MeetingResponse> findByPatientId(UUID patientId) {
+        return meetingRepository.findByPatientId(patientId)
+                .stream()
+                .map(MeetingMapper::toResponseDTO)
+                .toList();
+    }
+
+    public void cancelMeeting(String meetingId) {
+        Meeting meeting = meetingRepository.findById(meetingId).orElseThrow(() -> new MeetingNotFoundException(meetingId));
+
+        meeting.setStatus(MeetingStatus.CANCELLED);
+        meeting.setUpdatedAt(Instant.now());
+
+        meetingRepository.save(meeting);
+    }
 }
