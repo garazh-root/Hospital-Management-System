be609d81c5ecf67d0f5354f1862fecda72ec2990
Maksym2105 <maxdetrai02@gmail.com>
2026-03-05 17:23:37 +0200

    Added test coverage for controller, mapper classes

--- meeting-service/src/test/java/org/com/meetingservice/mapper/MeetingMapperTest.java
+++ meeting-service/src/test/java/org/com/meetingservice/mapper/MeetingMapperTest.java
@@ -1,8 +1,40 @@
 package org.com.meetingservice.mapper;
 
-import static org.junit.Assert.assertEquals;
+
+import org.com.meetingservice.additional.MeetingStatus;
+import org.com.meetingservice.dto.MeetingResponse;
+import org.com.meetingservice.model.Meeting;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MeetingMapperTest {
 
+    @Test
+    void toResponseShouldCorrectlyMapMeeting () {
+        UUID doctorId = UUID.randomUUID();
+        UUID patientId = UUID.randomUUID();
 
+        LocalDateTime dateTime = LocalDateTime.of(2026, 6, 1, 9, 0, 0);
+
+        Meeting meeting = Meeting.builder()
+                .id("ddcf0ec6fc0e407ab460bc37")
+                .doctorId(doctorId)
+                .patientId(patientId)
+                .meetingDateTime(dateTime)
+                .durationOfMinutes(30)
+                .status(MeetingStatus.CONFIRMED)
+                .notes("Test")
+                .build();
+
+        MeetingResponse meetingResponse = MeetingMapper.toResponseDTO(meeting);
+
+        assertThat(meetingResponse.doctorId()).isEqualTo(doctorId.toString());
+        assertThat(meetingResponse.patientId()).isEqualTo(patientId.toString());
+        assertThat(meetingResponse.meetingDateTime()).isEqualTo(dateTime.toString());
+        assertThat(meetingResponse.durationOfMinutes()).isEqualTo(Integer.valueOf(30).toString());
+    }
 }
