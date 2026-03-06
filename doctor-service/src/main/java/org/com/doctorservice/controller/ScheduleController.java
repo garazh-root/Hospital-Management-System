a6eb57133f386ee4f50d990fe34ce89436fa50d4
Maksym2105 <maxdetrai02@gmail.com>
2026-03-06 22:46:59 +0200

    Added Open API for schedule service

--- doctor-service/src/main/java/org/com/doctorservice/controller/ScheduleController.java
+++ doctor-service/src/main/java/org/com/doctorservice/controller/ScheduleController.java
@@ -1,5 +1,7 @@
 package org.com.doctorservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.com.doctorservice.additional.CustomDayOfTheWeek;
 import org.com.doctorservice.dto.*;
 import org.com.doctorservice.service.ScheduleService;
@@ -13,6 +15,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/schedule")
+@Tag(name = "schedule", description = "API for managing schedule templates/overrides")
 public class ScheduleController {
 
     private ScheduleService scheduleService;
@@ -22,6 +25,7 @@ public class ScheduleController {
     }
 
     @PostMapping("/template/{doctorIdToCreateTemplate}")
+    @Operation(summary = "Create schedule template")
     public ResponseEntity<ScheduleTemplateResponse> createScheduleTemplate(
             @PathVariable UUID doctorIdToCreateTemplate,
             @RequestBody ScheduleTemplateRequest scheduleTemplateRequest
@@ -31,6 +35,7 @@ public class ScheduleController {
     }
 
     @PutMapping("/templates/{templateId}")
+    @Operation(summary = "Update schedule template")
     public ResponseEntity<ScheduleTemplateResponse> updateScheduleTemplate(
             @PathVariable UUID templateId,
             @RequestBody ScheduleTemplateRequest scheduleTemplateRequest
@@ -40,18 +45,21 @@ public class ScheduleController {
     }
 
     @DeleteMapping("/templates/{templateId}")
+    @Operation(summary = "Delete schedule template")
     public ResponseEntity<Void> deleteScheduleTemplate(@PathVariable UUID templateId) {
         scheduleService.deleteScheduleTemplate(templateId);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/templates/{doctorId}")
+    @Operation(summary = "Get schedule templates by doctor id")
     public ResponseEntity<List<ScheduleTemplateResponse>> getScheduleTemplatesByDoctorId(@PathVariable UUID doctorId) {
         List<ScheduleTemplateResponse> scheduleTemplateResponseList = scheduleService.getScheduleTemplates(doctorId);
         return ResponseEntity.ok().body(scheduleTemplateResponseList);
     }
 
     @GetMapping("/templates/byDay/{doctorId}")
+    @Operation(summary = "Get schedule templates by doctor id and day of the week")
     public ResponseEntity<List<ScheduleTemplateResponse>> getSchedulesByDoctorIdAndDayOfTheWeek(
             @PathVariable UUID doctorId,
             @RequestParam CustomDayOfTheWeek customDayOfTheWeek
@@ -63,6 +71,7 @@ public class ScheduleController {
     }
 
     @PostMapping("/override/{doctorIdToCreateOverride}")
+    @Operation(summary = "Create schedule override")
     public ResponseEntity<ScheduleOverrideResponse> createScheduleOverride(
             @PathVariable UUID doctorIdToCreateOverride, @RequestBody ScheduleOverrideRequest scheduleOverrideRequest
     ) {
@@ -71,12 +80,14 @@ public class ScheduleController {
     }
 
     @DeleteMapping("/override/{overrideId}")
+    @Operation(summary = "Delete schedule override")
     public ResponseEntity<Void> deleteScheduleOverride(@PathVariable UUID overrideId) {
         scheduleService.deleteScheduleOverride(overrideId);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/overrides/range/{doctorId}")
+    @Operation(summary = "Get schedule overrides by doctor id an date between")
     public ResponseEntity<List<ScheduleOverrideResponse>> getSchedulesOverridesByDoctorIdAndDateBetween(
             @PathVariable UUID doctorId,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
@@ -89,6 +100,7 @@ public class ScheduleController {
     }
 
     @GetMapping("/overrides/{doctorId}")
+    @Operation(summary = "Get schedule overrides by doctor id")
     public ResponseEntity<List<ScheduleOverrideResponse>> getSchedulesOverridesByDoctorId (
             @PathVariable UUID doctorId
     ) {
@@ -97,6 +109,7 @@ public class ScheduleController {
     }
 
     @GetMapping("/data")
+    @Operation(summary = "Get schedule data by doctor id and date between")
     public ResponseEntity<ScheduleResponse> getScheduleDataByDoctorIdAndDateBetween(
             @RequestParam UUID doctorId,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
