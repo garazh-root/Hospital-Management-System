7117c2bee4bcd022bd8b77a0ff4f3888ff247665
Maksym2105 <maxdetrai02@gmail.com>
2026-04-21 17:29:45 +0300

    Added jwt filter to api gateway

---
+++ api-gateway/src/main/java/org/com/apigateway/filter/JwtAuthGatewayFilterFactory.java
@@ -0,0 +1,64 @@
+package org.com.apigateway.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthGatewayFilterFactory extends
+        AbstractGatewayFilterFactory<JwtAuthGatewayFilterFactory.Config> {
+    public JwtAuthGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            String authHeader = exchange.getRequest()
+                    .getHeaders()
+                    .getFirst(HttpHeaders.AUTHORIZATION);
+
+            if(authHeader == null || !authHeader.startsWith("Bearer ")){
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            try {
+                String token = authHeader.substring(7);
+
+                Claims claims = Jwts.parser()
+                        .verifyWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret)))
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
+
+                ServerHttpRequest request = exchange.getRequest().mutate()
+                        .header("X-User-Id", claims.getSubject())
+                        .header("X-User-role", claims.get("role", String.class))
+                        .build();
+
+                return chain.filter(exchange.mutate().request(request).build());
+
+            } catch (JwtException e) {
+                System.out.println("Jwt Validation Failed : " + e.getMessage());
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+        };
+    }
+
+    public static class Config {
+    }
+}
