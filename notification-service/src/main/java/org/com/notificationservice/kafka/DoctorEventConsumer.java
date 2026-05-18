b3c056479aa0a49c669122d6f4aa1c9b5757a526
Maksym2105 <maxdetrai02@gmail.com>
2026-05-18 15:00:10 +0300

    Adjusted doctor/patient event consumer classes

--- notification-service/src/main/java/org/com/notificationservice/kafka/DoctorEventConsumer.java
+++ notification-service/src/main/java/org/com/notificationservice/kafka/DoctorEventConsumer.java
@@ -2,7 +2,6 @@ package org.com.notificationservice.kafka;
 
 import lombok.extern.slf4j.Slf4j;
 import org.com.notificationservice.event.doctor.DoctorChangedStatusEvent;
-import org.com.notificationservice.event.doctor.DoctorCreatedEvent;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -11,17 +10,6 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @KafkaListener(topics = "doctor-topic", groupId = "notification")
 public class DoctorEventConsumer {
-
-    @KafkaHandler
-    public void onDoctorCreated(DoctorCreatedEvent event) {
-        log.info("Doctor Created Event | id : {} | firstName : {} | lastName : {} | email : {} | phoneNumber : {} | status : {} ",
-                event.id(),
-                event.firstName(),
-                event.lastName(),
-                event.email(),
-                event.phoneNumber(),
-                event.status());
-    }
 
     @KafkaHandler
     public void onDoctorUpdatedStatus(DoctorChangedStatusEvent event) {
