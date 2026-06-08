6dcc3006e87be31e5fcfdb76898aad3b200fd3c0
Maksym2105 <maxdetrai02@gmail.com>
2026-06-08 12:43:37 +0300

    Added annotation for AutomateScheduling class in meeting service

--- meeting-service/src/main/java/org/com/meetingservice/service/scheduling/AutomateScheduling.java
+++ meeting-service/src/main/java/org/com/meetingservice/service/scheduling/AutomateScheduling.java
@@ -1,10 +1,16 @@
 package org.com.meetingservice.service.scheduling;
 
+import lombok.extern.slf4j.Slf4j;
 import org.com.meetingservice.service.MeetingService;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+
 @Component
+@EnableScheduling
+@Slf4j
 public class AutomateScheduling {
 
     private final MeetingService meetingService;
@@ -15,6 +21,7 @@ public class AutomateScheduling {
 
     @Scheduled(fixedRate = 60000)
     public void automateScheduling () {
+        log.info("Automate Scheduling enabled at {}", Instant.now());
         meetingService.autoCompleteMeeting();
     }
 }
